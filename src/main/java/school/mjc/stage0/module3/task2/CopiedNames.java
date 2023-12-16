8f11c6916c85d9062ff140739ac5838bca9afb61
Constantine <77935386+velteren@users.noreply.github.com>
2023-12-16 15:05:39 +0300

    refactor: naming convention

--- src/main/java/school/mjc/stage0/module3/task2/CopiedNames.java
+++ src/main/java/school/mjc/stage0/module3/task2/CopiedNames.java
@@ -3,12 +3,16 @@ package school.mjc.stage0.module3.task2;
 public class CopiedNames {
     public static void main(String[] args) {
         int variable = 0;
-        variable = 2000;
+        int variable1 = 2000;
         int value = 1000;
-        variable = 4000;
+        int variable2 = 4000;
         int value2 = 3000;
         int variables = 5000;
         variables = 7000;
-        value = 90000;
+        int value1 = 90000;
+        int int1 = 100000;
+        int myInteger = 1000000;
+        int myVar = 1234567;
+        int myInteger2 = 1234567;
     }
 }
